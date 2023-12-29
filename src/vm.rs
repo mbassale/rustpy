9a8c1617c4cc9edff2d4dc1a90a7b0426c272078
Marco Bassaletti <bassaletti@gmail.com>
2023-12-29 12:56:16 -0800

    [VM] Bytecode execution of Add, Sub, Mul, Div and string concatenation

--- src/vm.rs
+++ src/vm.rs
@@ -49,51 +49,13 @@ impl Vm {
                     self.stack.push(object);
                     self.ip += SIZE_INSTRUCTION + SIZE_INDEX;
                 }
-                Bytecode::Add => {
+                Bytecode::Add | Bytecode::Sub | Bytecode::Mul | Bytecode::Div => {
                     let rhs = self.stack.pop().unwrap();
                     let lhs = self.stack.pop().unwrap();
-
-                    let result = match lhs {
-                        Object::Integer(lhs_val) => match rhs {
-                            Object::Integer(rhs_val) => Object::Integer(lhs_val + rhs_val),
-                            Object::Float(rhs_val) => Object::Float(lhs_val as f64 + rhs_val),
-                            _ => {
-                                return Err(VmError::InvalidOperand(format!(
-                                    "TypeError: unsupported operand type(s) for +: 'int' and {:?}",
-                                    rhs
-                                )));
-                            }
-                        },
-                        Object::Float(lhs_val) => match rhs {
-                            Object::Integer(rhs_val) => Object::Float(lhs_val + rhs_val as f64),
-                            Object::Float(rhs_val) => Object::Float(lhs_val + rhs_val),
-                            _ => {
-                                return Err(VmError::InvalidOperand(format!(
-                                "TypeError: unsupported operand type(s) for +: 'float' and {:?}", rhs)
-                            ));
-                            }
-                        },
-                        Object::String(lhs_val) => {
-                            match rhs {
-                                Object::String(rhs_val) => Object::String(lhs_val + &rhs_val),
-                                _ => {
-                                    return Err(VmError::InvalidOperand(format!("TypeError: unsupported operand type(s) for +: 'str' and {:?}", rhs)));
-                                }
-                            }
-                        }
-                        _ => {
-                            return Err(VmError::InvalidOperand(format!(
-                                "Invalid operands for add: {:?} & {:?}",
-                                rhs, lhs
-                            )))
-                        }
-                    };
+                    let result = binary_op(&op, &lhs, &rhs)?;
                     self.stack.push(result);
                     self.ip += SIZE_INSTRUCTION;
                 }
-                Bytecode::Sub => {}
-                Bytecode::Mul => {}
-                Bytecode::Div => {}
                 _ => unimplemented!(),
             };
         }
@@ -104,4 +66,61 @@ impl Vm {
         };
         Ok(result)
     }
+}
+
+fn binary_op(op: &Bytecode, lhs: &Object, rhs: &Object) -> Result<Object, VmError> {
+    let unsupported_operand_types = || -> VmError {
+        VmError::InvalidOperand(format!(
+            "TypeError: unsupported operand type(s) for {:?}: {:?} and {:?}",
+            op, rhs, lhs
+        ))
+    };
+
+    let apply_i64_op = |lhs: i64, rhs: i64| -> Result<i64, VmError> {
+        let result = match op {
+            Bytecode::Add => lhs + rhs,
+            Bytecode::Sub => lhs - rhs,
+            Bytecode::Mul => lhs * rhs,
+            Bytecode::Div => lhs / rhs,
+            __ => return Err(unsupported_operand_types()),
+        };
+        Ok(result)
+    };
+
+    let apply_f64_op = |lhs: f64, rhs: f64| -> Result<f64, VmError> {
+        let result = match op {
+            Bytecode::Add => lhs + rhs,
+            Bytecode::Sub => lhs - rhs,
+            Bytecode::Mul => lhs * rhs,
+            Bytecode::Div => lhs / rhs,
+            _ => return Err(unsupported_operand_types()),
+        };
+        Ok(result)
+    };
+
+    let apply_string_op = |lhs: &str, rhs: &str| -> Result<String, VmError> {
+        match op {
+            Bytecode::Add => Ok(lhs.to_string() + rhs),
+            _ => return Err(unsupported_operand_types()),
+        }
+    };
+
+    let result = match lhs {
+        Object::Integer(lhs_val) => match rhs {
+            Object::Integer(rhs_val) => Object::Integer(apply_i64_op(*lhs_val, *rhs_val)?),
+            Object::Float(rhs_val) => Object::Float(apply_f64_op(*lhs_val as f64, *rhs_val)?),
+            _ => return Err(unsupported_operand_types()),
+        },
+        Object::Float(lhs_val) => match rhs {
+            Object::Integer(rhs_val) => Object::Float(apply_f64_op(*lhs_val, *rhs_val as f64)?),
+            Object::Float(rhs_val) => Object::Float(apply_f64_op(*lhs_val, *rhs_val)?),
+            _ => return Err(unsupported_operand_types()),
+        },
+        Object::String(lhs_val) => match rhs {
+            Object::String(rhs_val) => Object::String(apply_string_op(lhs_val, rhs_val)?),
+            _ => return Err(unsupported_operand_types()),
+        },
+        _ => return Err(unsupported_operand_types()),
+    };
+    Ok(result)
 }
